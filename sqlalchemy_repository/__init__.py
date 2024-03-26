2ef057b12f76a0b3d06a5844708334a1d2651d0b
Javier Valero Cejudo <javalce29@gmail.com>
2024-03-26 23:24:26 +0100

    Add Entity class to SQLAlchemy repository

--- sqlalchemy_repository/__init__.py
+++ sqlalchemy_repository/__init__.py
@@ -1,5 +1,7 @@
+from .entity import Entity
 from .manager import DatabaseManager
 
 __all__ = [
+    "Entity",
     "DatabaseManager",
 ]
---
+++ tests/test_entity.py
@@ -0,0 +1,20 @@
+import pytest
+from sqlalchemy import Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, clear_mappers, mapped_column
+
+from sqlalchemy_repository import Entity
+
+
+@pytest.fixture(autouse=True)
+def before_each():
+    yield
+    Entity.metadata.clear()
+    clear_mappers()
+
+
+def test_entity():
+    class DummyModel(Entity):
+        __tablename__ = "dummy"
+        id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    assert issubclass(DummyModel, DeclarativeBase)
