2f2038090f705a4ec9d1d6ff7cb279ebbf99975d
Javier Valero Cejudo <javalce29@gmail.com>
2024-04-01 18:33:23 +0200

    Add docstrings to Entity, SQLAlchemyMiddleware, and SQLAlchemyRepository classes

--- sqlalchemy_repository/ext/fastapi/middleware.py
+++ sqlalchemy_repository/ext/fastapi/middleware.py
@@ -7,11 +7,32 @@ from ...manager import DatabaseManager
 
 
 class SQLAlchemyMiddleware(BaseHTTPMiddleware):
+    """
+    Middleware class for integrating SQLAlchemy with FastAPI.
+
+    This middleware manages the database session for each request by creating a new session
+    and closing it after the request is processed.
+
+    :param app: The ASGI application.
+    :param db: The DatabaseManager instance.
+    """
+
     def __init__(self, app: ASGIApp, db: DatabaseManager) -> None:
         super().__init__(app)
         self.db = db
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        """
+        Dispatch method that handles the request and response.
+
+        This method creates a new database session using the session context manager provided
+        by the DatabaseManager instance. It then calls the next middleware or the application
+        itself to handle the request. After the response is received, the session is closed.
+
+        :param request: The incoming request.
+        :param call_next: The next middleware or application to call.
+        :return: The response.
+        """
         with self.db.session_ctx():
             response = await call_next(request)
         return response
